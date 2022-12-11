6ddce4a252f31a4d1871a63684494609525b727f
RZZT <raulsanchezzt@duck.com>
2022-12-11 16:10:37 +0100

    🦪 JavaScript exercise 1.3

---
+++ WebDev/Midudev - Free Bootcamp FullStack/part1/courseinfo/src/App.js
@@ -0,0 +1,55 @@
+const Header = ({ course }) => {
+  return <h1>{course}</h1>;
+};
+
+const Content = ({ part1, part2, part3 }) => {
+  return (
+    <div>
+      <Part part={part1.name} exercises={part1.exercises} />
+      <Part part={part2.name} exercises={part2.exercises} />
+      <Part part={part3.name} exercises={part3.exercises} />
+    </div>
+  );
+};
+
+const Part = ({ part, exercises }) => {
+  return (
+    <p>
+      {part} {exercises}
+    </p>
+  );
+};
+
+const Total = ({ exercises1, exercises2, exercises3 }) => {
+  return <p>Number of exercises: {exercises1 + exercises2 + exercises3}</p>;
+};
+
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Total
+        exercises1={part1.exercises}
+        exercises2={part2.exercises}
+        exercises3={part3.exercises}
+      />
+    </div>
+  );
+};
+
+export default App;
