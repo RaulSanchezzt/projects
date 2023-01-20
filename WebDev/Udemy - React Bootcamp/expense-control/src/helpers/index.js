b4d1f7eb18ff6405ead3b9f8a38381d43296a698
RZZT <raulsanchezzt@duck.com>
2023-01-20 21:29:54 +0100

    🆘 Upload helpers functions

---
+++ WebDev/Udemy - React Bootcamp/expense-control/src/helpers/index.js
@@ -0,0 +1,16 @@
+export const generateId = () => {
+  const random = Math.random().toString(36).substring(2);
+  const date = Date.now().toString(36);
+  return random + date;
+};
+
+export const formatDate = (date) => {
+  const newDate = new Date(date);
+  const config = {
+    year: "numeric",
+    month: "long",
+    day: "2-digit",
+  };
+
+  return newDate.toLocaleDateString("en-US", config);
+};
