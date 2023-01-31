beddfc704ee6a05b0e3431118eb35da852a5360b
RZZT <raulsanchezzt@duck.com>
2023-01-31 21:54:02 +0100

    🦜 Update lottery

--- Blockchain/Udemy - Blockchain Bootcamp Zero to Expert/Lottery/loteria.sol
+++ Blockchain/Udemy - Blockchain Bootcamp Zero to Expert/Lottery/loteria.sol
@@ -24,8 +24,96 @@ contract loteria is ERC20, Ownable {
 
     // Registro del usuario
     mapping(address => address) public usuario_contract;
+
+    // Precio de los tokens
+    function precioTokens(uint256 _numTokens) internal pure returns (uint256) {
+        return _numTokens * (1 ether);
+    }
+
+    // Visualizacion del balance de tokens ERC-20 de un usuario
+    function balanceTokens(address _account) public view returns (uint256) {
+        return balanceOf(_account);
+    }
+
+    // Visualizacion del balance de tokens ERC-20 del Smart Contract
+    function balanceTokensSC() public view returns (uint256) {
+        return balanceOf(address(this));
+    }
+
+    // Visualizacion del balance de ethers del Smart Contract
+    // 1 ether = 1^18 wei
+    function balanceEthersSC() public view returns (uint256) {
+        return address(this).balance / 10 ** 18;
+    }
+
+    // Generacion de nuevos Tokens ERC-20
+    function mint(uint256 _cantidad) public onlyOwner {
+        _mint(address(this), _cantidad);
+    }
+
+    // Registro de usuarios
+    function registrar() internal {
+        address addr_personal_contract = address(
+            new boletosNFTs(msg.sender, address(this), nft)
+        );
+        usuario_contract[msg.sender] = addr_personal_contract;
+    }
+
+    // Informacion de un usuario
+    function usersInfo(address _account) public view returns (address) {
+        return usuario_contract[_account];
+    }
 }
 
+// Smart Contract de NFTs
 contract mainERC721 is ERC721 {
-    constructor() ERC721("Loteria", "SLN") {}
+    address public direccionLoteria;
+
+    constructor() ERC721("Loteria", "SLN") {
+        direccionLoteria = msg.sender;
+    }
+
+    // Creacion de NFTs
+    function safeMint(address _propietario, uint256 _boleto) public {
+        require(
+            msg.sender == loteria(direccionLoteria).usersInfo(_propietario),
+            "No tienes permisos para ejecutar esta funcion."
+        );
+        _safeMint(_propietario, _boleto);
+    }
+}
+
+contract boletosNFTs {
+    // Datos relevantes del propietario
+    struct Owner {
+        address direccionPropietario;
+        address contratoPadre;
+        address contratoNFT;
+        address contratoUsuario;
+    }
+    // Estructura de datos de tipo Owner
+    Owner public propietario;
+
+    // Constructor del Smart Contract (hijo)
+    constructor(
+        address _propietario,
+        address _contratoPadre,
+        address _contratoNFT
+    ) {
+        propietario = Owner(
+            _propietario,
+            _contratoPadre,
+            _contratoNFT,
+            address(this)
+        );
+    }
+
+    // Conversion de los numeros de los boletos de loteria
+    function mintBoleto(address _propietario, uint _boleto) public {
+        require(
+            msg.sender == propietario.contratoPadre,
+            "No tienes permisos para ejecutar esta funcion."
+        );
+        mainERC721(propietario.contratoNFT).safeMint(_propietario, _boleto);
+    }
 }
