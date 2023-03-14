ce9fa284b210c70e7c27536cc7d3807d9a616e4f
RZZT <raulsanchezzt@duck.com>
2023-03-14 15:13:29 +0100

    🏪 Update shop

--- WebDev/Udemy - React Bootcamp/guitarla-remix/app/routes/shop.jsx
+++ WebDev/Udemy - React Bootcamp/guitarla-remix/app/routes/shop.jsx
@@ -1,7 +1,26 @@
-function Shop() {
-  return (
-    <div>Shop</div>
-  )
+import { useLoaderData } from "@remix-run/react";
+import { getGuitars } from "~/models/guitars.server";
+import Guitar from "../components/guitar";
+
+export async function loader() {
+  const guitars = await getGuitars();
+  return guitars.data;
 }
 
-export default Shop
+function Shop() {
+  const guitars = useLoaderData();
+  return (
+    <main className="container">
+      <h2 className="heading">Our Collection</h2>
+      {guitars?.length && (
+        <div className="guitars-grid">
+          {guitars.map((guitar) => (
+            <Guitar key={guitar?.id} guitar={guitar?.attributes} />
+          ))}
+        </div>
+      )}
+    </main>
+  );
+}
+
+export default Shop;
