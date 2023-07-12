4e6912f599dfa0f3527d87cd47064fafdc24db06
RZZT <raulsanchezzt@duck.com>
2023-07-12 13:26:44 +0200

    🏪 Update NFT Marketplace

---
+++ Blockchain/Udemy - Blockchain Bootcamp Zero to Expert/NFT Marketplace/src/frontend/components/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+ 
+function App() {
+  return (
+    <div>
+      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+        <a
+          className="navbar-brand col-sm-3 col-md-2 ms-3"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          DApp
+        </a>
+      </nav>
+      <div className="container-fluid mt-5">
+        <div className="row">
+          <main role="main" className="col-lg-12 d-flex text-center">
+            <div className="content mx-auto mt-5">
+
+            </div>
+          </main>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
